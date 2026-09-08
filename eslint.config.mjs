fcaeaec3b0fd25894c0bfdd860448c15e9e0b026
Brodi <brodamousmaximus@gmail.com>
2026-09-07 17:48:58 -0700

    Upgrade dependencies and verify SceneGuard build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,9 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
-
-export default eslintConfig;
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+]);
 
